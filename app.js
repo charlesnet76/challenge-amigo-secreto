dd497152beef8f43a6c4450f1db2513378f34faa
Carlos Medina <76129110+charlesnet76@users.noreply.github.com>
2025-03-14 18:24:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function sortearAmigo() {
     alert("Por favor, inserte un nombre antes de sortear");
   } else {
     let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
-    let resultadoHTML = document.querySelector("#resultado");
+    let resultadoHTML = document.querySelector("prueba a escojer#resultado");
     resultadoHTML.innerHTML = amigos[indiceAmigo];
   }
 }
